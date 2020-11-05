4d9fbb3c852ff91008069dca1ee82ac46c05ceb4
Praja510 <moh.prajadinata@gmail.com>
2020-11-05 19:23:06 +0700

    newest

--- app/src/main/java/com/mprajadinata/happyplace/MainActivity.kt
+++ app/src/main/java/com/mprajadinata/happyplace/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.mprajadinata.happyplace
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mprajadinata.happyplace.adapter.HappyPlaceAdapter
@@ -17,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, ADD_PLACE_ACTIVITY_REQUEST_CODE)
         }
 
         getHappyPlacesListFromLocalDB()
@@ -45,5 +47,22 @@ class MainActivity : AppCompatActivity() {
         rv_happy_places_list.setHasFixedSize(true)
         val placesAdapter = HappyPlaceAdapter(this, happyPlaceList)
         rv_happy_places_list.adapter = placesAdapter
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == ADD_PLACE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                getHappyPlacesListFromLocalDB()
+            } else {
+                Log.e("Activity", "Cancelled or Back Pressed")
+            }
+
+        }
+    }
+
+    companion object {
+        private const val ADD_PLACE_ACTIVITY_REQUEST_CODE = 1
     }
 }
